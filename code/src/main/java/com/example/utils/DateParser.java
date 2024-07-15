75ba5949d65881b07c1e45228a55970a997e54db
Kevyn Marins <kevynmarins@gmail.com>
2024-07-15 20:04:01 +0100

    fix getEnd method

--- code/src/main/java/com/example/utils/DateParser.java
+++ code/src/main/java/com/example/utils/DateParser.java
@@ -9,23 +9,30 @@ import java.util.List;
 public class DateParser {
     /**
      * Gets the end time for the window function.
-     * This method returns the timestamp of the last translation in the list,
-     * truncated to the nearest minute, and then adds one minute.
+     * This method returns the timestamp of the last translation in the list.
+     * If the timestamp is exactly on the minute, it returns it as is.
+     * Otherwise, it truncates the timestamp to remove the seconds and nanoseconds,
+     * and then adds one minute.
      *
      * @param translationList the list of translations
-     * @return the end time, truncated to the nearest minute and plus one minute
+     * @return the end time; either the original timestamp if it's on the minute,
+     *         or the timestamp with seconds removed, plus one minute
      */
     public static LocalDateTime getEnd(List<Translation> translationList) {
-        return translationList.getLast().getTimestamp().truncatedTo(ChronoUnit.MINUTES).plusMinutes(1);
+        LocalDateTime lastTimestamp = translationList.getLast().getTimestamp();
+
+        return lastTimestamp.getSecond() == 0
+                ? lastTimestamp
+                : lastTimestamp.truncatedTo(ChronoUnit.MINUTES).plusMinutes(1);
     }
 
     /**
      * Gets the start time for the window function.
      * This method returns the timestamp of the first translation in the list,
-     * truncated to the nearest minute.
+     * with the seconds and nanoseconds removed.
      *
      * @param translationList the list of translations
-     * @return the start time, truncated to the nearest minute
+     * @return the start time, with the seconds and nanoseconds removed
      */
     public static LocalDateTime getStart(List<Translation> translationList) {
         return translationList.getFirst().getTimestamp().truncatedTo(ChronoUnit.MINUTES);
